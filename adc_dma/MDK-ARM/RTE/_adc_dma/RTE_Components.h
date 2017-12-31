e95cfe37f42de046ce7ae99d723b886d5214af82
Bill-Park <tkddn0916@gmail.com>
2017-12-31 19:28:41 +0900

    add adc_dma

---
+++ adc_dma/MDK-ARM/RTE/_adc_dma/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'adc_dma' 
+ * Target:  'adc_dma' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
